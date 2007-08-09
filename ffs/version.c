99ad23ec693b4d30b656a034400b9b09ab2723e2
Chaos User <chaos@cc.gatech.edu>
2007-08-09 20:47:13 +0000

    normal version update
    git-svn-id: http://svn.research.cc.gatech.edu/kaos/ffs/trunk@3383 8ba6a8b9-845b-0410-94f0-974be2f5df47

--- ffs/version.c
+++ ffs/version.c
@@ -16,7 +16,7 @@
 #include "unix_defs.h"
 #endif
 
-static char *PBIO_version = "PBIO Version 4.0.16 -- Wed May 16 12:26:56 EDT 2007\n";
+static char *PBIO_version = "PBIO Version 4.0.17 -- Thu Aug  9 16:47:13 EDT 2007\n";
 
 void
 IOprint_version()
