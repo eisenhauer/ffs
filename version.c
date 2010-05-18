44be4f368f61af881f70d00f58e666ffa61c194e
Greg Eisenhauer <eisen@cc.gatech.edu>
2010-05-18 18:03:55 +0000

    Fix version.c
    git-svn-id: http://svn.research.cc.gatech.edu/kaos/ffs/trunk@8298 8ba6a8b9-845b-0410-94f0-974be2f5df47

--- version.c
+++ version.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-static char *FFS_version = "FFS Version 1.1.18 rev. 8277  -- 2010-04-30 00:51:58 -0400 (Fri, 30 Apr 2010)0)0)0)0)\n";
+static char *FFS_version = "FFS Version 1.1.18 rev. 8277  -- 2010-04-30 00:51:58 -0400 (Fri, 30 Apr 2010)\n";
 
 void
 FFSprint_version()
