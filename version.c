b5f40919466da111ffb8c068088521f3584d5f0f
Greg Eisenhauer <eisen@cc.gatech.edu>
2010-05-20 15:39:42 +0000

    svn_version_c_update script was broken.  Fix version.c so we can try it again
    git-svn-id: http://svn.research.cc.gatech.edu/kaos/ffs/trunk@8313 8ba6a8b9-845b-0410-94f0-974be2f5df47

--- version.c
+++ version.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-static char *FFS_version = "FFS Version 1.1.19 rev. 8311  -- 2010-05-20 11:34:40 -0400 (Thu, 20 May 2010)0) \n";
+static char *FFS_version = "FFS Version 1.1.19 rev. 8311  -- 2010-05-20 11:34:40 -0400 (Thu, 20 May 2010)\n";
 
 void
 FFSprint_version()
