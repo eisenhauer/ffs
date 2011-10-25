cfe9a76f12f0fa979ec4d810afe51c310f3f49b4
Chaos User <chaos@cc.gatech.edu>
2011-10-25 06:00:18 +0000

    Normal version update
    git-svn-id: http://svn.research.cc.gatech.edu/kaos/ffs/trunk@10350 8ba6a8b9-845b-0410-94f0-974be2f5df47

--- version.c
+++ version.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-static char *FFS_version = "FFS Version 1.1.48 rev. 10329  -- 2011-10-20 13:36:07 -0400 (Thu, 20 Oct 2011)\n";
+static char *FFS_version = "FFS Version 1.1.49 rev. 10348  -- 2011-10-24 21:18:32 -0400 (Mon, 24 Oct 2011)\n";
 
 void
 FFSprint_version()
