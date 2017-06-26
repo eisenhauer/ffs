46598ec9ef8885a1d4f44170744c052deb0096c5
Greg Eisenhauer <eisen@cc.gatech.edu>
2017-06-26 10:55:24 -0400

    update version

--- version.c
+++ version.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-static char *FFS_version = "FFS Version 1.1.236 rev. 26297  -- 2016-11-18 13:51:24 -0500 (Fri, 18 Nov 2016)\n";
+static char *FFS_version = "FFS Version 1.1.237  - Mon Jun 26 10:54:41 EDT 2017\n";
 
 void
 FFSprint_version()
