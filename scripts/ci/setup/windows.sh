e83b73a4e3d70266808d44fbcd09c5b589e0050f
Greg Eisenhauer <eisen@cc.gatech.edu>
2025-08-22 11:03:34 -0400

    wip

--- scripts/ci/setup/windows.sh
+++ scripts/ci/setup/windows.sh
@@ -2,7 +2,7 @@ reg add "/?"
 reg query "HKEY_LOCAL_MACHINE\\System\\CurrentControlSet\\Control\\Session Manager"
 reg query "HKEY_LOCAL_MACHINE\\System\\CurrentControlSet\\Control\\Session Manager\\SafeDllSearchMode" || true
 echo "setting key"
-reg add "HKEY_LOCAL_MACHINE\\System\\CurrentControlSet\\Control\\Session Manager\\" "//v" "SafeDllSearchMode" "//d" "0"
+reg add "HKEY_LOCAL_MACHINE\\System\\CurrentControlSet\\Control\\Session Manager\\" "//v" "SafeDllSearchMode" "//d" "0" "//t" "REG_DWORD"
 echo "checking key"
 reg query "HKEY_LOCAL_MACHINE\\System\\CurrentControlSet\\Control\\Session Manager\\SafeDllSearchMode" || true
 echo "checking key2"
